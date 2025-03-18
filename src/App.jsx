18bd086e8e1f4c447597995fa916f22456934395
Khushiimaheshwari <142376177+Khushiimaheshwari@users.noreply.github.com>
2025-03-18 17:35:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return(
+    <div className='flex justify-center p-10'>
+      <h1 className='text-5xl font-bold'>Lets Start</h1>
+    </div>
+  )
+}
+
+export default App
